42bf665823682897cb02c949d7a0933be71b98a6
ssshooter <ssshooterx@gmail.com>
2025-06-01 16:06:26 +0800

    chore: remove main application files and router configuration

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './style.less'
-import '@/icon/iconfont.css'
-import App from './App.vue'
-import router from './router'
-import i18n from './i18n'
-
-
-createApp(App).use(i18n).use(router).mount('#app')
