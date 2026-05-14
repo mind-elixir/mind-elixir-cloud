ba6f796cedac3c2e364a19af2ffc81e97b1d2a2b
ssshooter <ssshooterx@gmail.com>
2026-05-14 23:05:02 +0800

    refactor: remove unused skeleton components and update 404 page to use UI Button component

--- src/app/[locale]/(needNavbar)/404/page.tsx
+++ src/app/[locale]/(needNavbar)/404/page.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 export default function PageNotFound() {
   return (
@@ -16,9 +17,9 @@ export default function PageNotFound() {
         />
         <h1 className="text-4xl font-bold mb-4">Page Not Found</h1>
         <p className="text-lg mb-8">The page you are looking for does not exist.</p>
-        <Link href="/" className="btn btn-primary">
-          Go Home
-        </Link>
+        <Button asChild>
+          <Link href="/">Go Home</Link>
+        </Button>
       </div>
     </div>
   )
--- src/app/[locale]/share/[id]/components/index.ts
+++ src/app/[locale]/share/[id]/components/index.ts
@@ -1,5 +1,3 @@
-export { MindMapSkeleton } from './MindMapSkeleton'
-export { PageSkeleton } from './PageSkeleton'
 export { ErrorState } from './ErrorState'
 export { ShareHeader } from './ShareHeader'
 export { ViewContent } from './ViewContent'
